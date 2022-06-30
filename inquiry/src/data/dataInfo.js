432c0ae873f39cc048e4a673a03dcafc4ab517cd
tkomitov <Vqcuba43>
2022-06-30 16:27:14 +0300

    code clearance

---
+++ inquiry/src/data/dataInfo.js
@@ -0,0 +1,41 @@
+export default class DataInfo {
+
+    static getDocInfo() {
+        return [
+            {
+              name: "Ivan",
+              o1: 0,
+              o2: 0,
+              o3: 0,
+              o4: 0,
+              o5: 0,
+              o6: 0,
+              o7: 0
+            },
+            {
+              name: "Pesho",
+              o1: 0,
+              o2: 0,
+              o3: 0,
+              o4: 0,
+              o5: 0,
+              o6: 0,
+              o7: 0
+            },
+            {
+              name: "Gosho",
+              o1: 0,
+              o2: 0,
+              o3: 0,
+              o4: 0,
+              o5: 0,
+              o6: 0,
+              o7: 0
+            }
+        ]
+    }
+
+    static getOpsInfo() {
+        
+    }
+}
